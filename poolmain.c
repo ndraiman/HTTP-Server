730779d0a3f350ec21d8f63d074cfaebf5fb03c8
ND88 Vivobook Ubuntu <ndraiman@gmail.com>
2015-12-22 22:16:48 +0200

    added debug prints for threadpool and a small tester for it

---
+++ poolmain.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include "threadpool.h"
+
+#define DEBUG 1
+#define debug_print(fmt, ...) \
+           do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
+
+int avoda(void*);
+
+int main() {
+
+        srand(time(NULL));
+        int num_of_threads = 5;
+        int num_of_jobs = 20;
+
+        debug_print("Main - %s\n", "Creating pool");
+        threadpool* pool = create_threadpool(num_of_threads);
+        int i;
+        for(i = 0; i < num_of_threads; i++) {
+                debug_print("thread[%d] tid = %d\n", i, (int)pool->threads[i]);
+        }
+
+        int y[num_of_jobs];
+
+        debug_print("\n%s\n", "****************************************");
+        for(i = 0; i < num_of_jobs; i ++) {
+
+                y[i] = rand();
+                debug_print("Main - dispatching job with x = %d\n", y[i]);
+                dispatch(pool, avoda, (void*)(&(y[i])));
+
+        }
+        debug_print("\n%s\n", "****************************************");
+
+        debug_print("Main - %s\n", "going to sleep");
+        sleep(5);
+        debug_print("Main - %s\n", "woke up");
+
+        debug_print("Main - %s\n", "destroying pool");
+        destroy_threadpool(pool);
+
+}
+
+
+int avoda(void* pong) {
+        debug_print("*****Sleeping on the job - tid: %d\n", (int)pthread_self());
+        sleep(5);
+        debug_print("*****!!!! JOB ROUTINE BEING DONE - tid: %d\n", (int)pthread_self());
+        debug_print("*****pong = %d\n", *((int*)pong));
+        return 0;
+}
--- threadpool.c
+++ threadpool.c
@@ -3,6 +3,10 @@
 #include <pthread.h>
 #include "threadpool.h"
 
+#define DEBUG 0
+#define debug_print(fmt, ...) \
+           do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
 pthread_t* initThreads(threadpool*, int);
 void enqueue_job(threadpool*, work_t*);
 
@@ -11,7 +15,7 @@ void enqueue_job(threadpool*, work_t*);
 /******************************************************************************/
 
 threadpool* create_threadpool(int num_threads_in_pool) {
-
+        debug_print("%s\n", "create_threadpool");
         if(num_threads_in_pool <= 0 || num_threads_in_pool > MAXT_IN_POOL)
                 return NULL;
 
@@ -76,14 +80,14 @@ pthread_t* initThreads(threadpool* pool, int num_of_threads) {
 
 
 void dispatch(threadpool* from_me, dispatch_fn dispath_to_here, void* arg) {
-
+        debug_print("%s\n", "dispatch");
         pthread_mutex_lock(&from_me->qlock);
 
         if(from_me->dont_accept) {
                 pthread_mutex_unlock(&from_me->qlock);
                 return;
         }
-
+        debug_print("\t%s\n", "creating new job");
         work_t* new_job = (work_t*)calloc(1, sizeof(work_t));
         if(new_job == NULL) {
                 pthread_mutex_unlock(&from_me->qlock);
@@ -96,7 +100,7 @@ void dispatch(threadpool* from_me, dispatch_fn dispath_to_here, void* arg) {
         new_job->next = NULL;
 
         enqueue_job(from_me, new_job);
-
+        debug_print("\t%s\n", "done");
         pthread_mutex_unlock(&from_me->qlock);
 }
 
@@ -105,9 +109,9 @@ void dispatch(threadpool* from_me, dispatch_fn dispath_to_here, void* arg) {
 /*********************************/
 
 void enqueue_job(threadpool* pool, work_t* job) {
-
+        debug_print("\t%s\n", "enqueue_job");
         if(pool->qsize == 0) {
-
+                debug_print("\t\t%s\n", "1st job");
                 pool->qhead = job;
                 pool->qtail = job;
                 pool->qsize++;
@@ -127,30 +131,34 @@ void enqueue_job(threadpool* pool, work_t* job) {
 /******************************************************************************/
 
 void* do_work(void* p) {
-
+        debug_print("do_work - tid = %d\n", (int)pthread_self());
         if(p == NULL)
                 return 0;
 
         threadpool* pool = (threadpool*) p;
 
         while(1) {
-
+                debug_print("\t attemping mutex - tid = %d\n", (int)pthread_self());
                 pthread_mutex_lock(&pool->qlock);
-
+                debug_print("\t i got mutex - tid = %d\n", (int)pthread_self());
                 if(pool->shutdown) {
                         pthread_mutex_unlock(&pool->qlock);
+                        debug_print("\tshutdown 1 - tid = %d\n", (int)pthread_self());
                         return 0;
                 }
 
-                if(!(pool->qsize))
+                if(!(pool->qsize)) {
+                        debug_print("\tqueue empty -> waiting - tid = %d\n", (int)pthread_self());
                         pthread_cond_wait(&pool->q_empty, &pool->qlock);
+                }
 
                 if(pool->shutdown) {
                         pthread_mutex_unlock(&pool->qlock);
+                        debug_print("\tshutdown 2 - tid = %d\n", (int)pthread_self());
                         return 0;
                 }
 
-
+                debug_print("\tdequeueing job - tid = %d\n", (int)pthread_self());
                 //dequeue job
                 work_t* job = pool->qhead;
                 pool->qhead = job->next;
@@ -158,14 +166,18 @@ void* do_work(void* p) {
 
 
                 //notify destroy function
-                if(!pool->qsize && pool->dont_accept)
+                if(!pool->qsize && pool->dont_accept) {
+                        debug_print("\nnotifying destory - tid = %d\n", (int)pthread_self());
                         pthread_cond_signal(&pool->q_not_empty);
-
+                }
+                debug_print("\tunlocking mutex - tid = %d\n", (int)pthread_self());
                 pthread_mutex_unlock(&pool->qlock);
 
+                debug_print("\trunning job - tid = %d\n", (int)pthread_self());
                 //run job
                 job->routine(job->arg);
                 free(job);
+                debug_print("\tDone! - tid = %d\n", (int)pthread_self());
         }
 }
 
@@ -175,25 +187,30 @@ void* do_work(void* p) {
 
 
 void destroy_threadpool(threadpool* destroyme) {
-
+        debug_print("%s\n", "destroy_threadpool");
         pthread_mutex_lock(&destroyme->qlock);
 
         destroyme->dont_accept = 1;
 
         //wait until queue is empty
-        if(destroyme->qsize != 0)
+        if(destroyme->qsize != 0) {
+                debug_print("\t%s\n", "queue isnt empty -> waiting");
                 pthread_cond_wait(&destroyme->q_not_empty, &destroyme->qlock);
+        }
 
+        debug_print("%s\n", "queue is empty");
         destroyme->shutdown = 1;
         //wake sleeping threads
         pthread_cond_broadcast(&destroyme->q_empty);
 
         pthread_mutex_unlock(&destroyme->qlock);
         int i;
-        for(i = 0; i < destroyme->num_threads; i++)
+        for(i = 0; i < destroyme->num_threads; i++) {
+                debug_print("waiting on thread #%d, tid: %d\n", i, (int)destroyme->threads[i]);
                 pthread_join(destroyme->threads[i], NULL);
+        }
 
-
+        debug_print("%s\n", "destroying pool");
         pthread_mutex_destroy(&destroyme->qlock);
         pthread_cond_destroy(&destroyme->q_empty);
         pthread_cond_destroy(&destroyme->q_not_empty);
